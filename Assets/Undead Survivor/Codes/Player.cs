09b39eba4d2ae5a3f5f73895b00692031c0fadb4
정재하 <jjh4039@naver.com>
2025-08-08 16:48:37 +0900

    2주차 완 - v.0.0.3

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    Rigidbody2D rigid;
+    
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}
