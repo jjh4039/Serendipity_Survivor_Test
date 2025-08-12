1a54be811b9fb3d59542e5393fc15e80353869c8
정재하 <jjh4039@naver.com>
2025-08-12 14:27:54 +0900

    3주차 완 - v.0.0.5
    3주차 애니메이션 완료
    01 과제 테스트 끝

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // ´Ð³×ÀÓ - Serendipity Unity Survivor 01
+
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
-    
+    SpriteRenderer spriter;
+    Animator anim;
+
     void Awake()
     {
+        anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -22,4 +29,15 @@ public class Player : MonoBehaviour
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
     }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
 }
+
