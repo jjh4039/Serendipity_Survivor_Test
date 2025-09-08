4c597ba09a9849dc29d2c2c34feaa186d0f358d5
정재하 <jjh4039@naver.com>
2025-09-08 17:33:32 +0900

    5주차 + 6주차 10분

---
+++ Assets/Undead Survivor/Codes/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLive) return; // 살아있지 않으면 동작하지 않음
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.linearVelocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive) return; // 살아있지 않으면 동작하지 않음
+
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+}
